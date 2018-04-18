82b12d90ccf5d422cdc6674017f4a85fff79f494
Martin Cooper <34508946+MCooper24@users.noreply.github.com>
2018-04-18 22:41:26 +0100

    Task 1 Possibly Complete
    The sum might be finished now, but task two is still WIP.

--- FIRsum.m
+++ FIRsum.m
@@ -1,8 +1,20 @@
-% UP736129 & UP504815
+%% TASK 1
+
 function Y = FIRsum(x,h)
 
-N = length(x);
-    
-for k=0:(N-1)
-    Y(n) = h(k)*x(n-k);
+% Calculates the FIR filter.
+
+m = length(x);
+n = length(h);
+X = [x,zeros(1,n)];
+H = [h,zeros(1,m)];
+
+for i=1:n+m-1
+    Y(i)=0;
+    for j=1:m
+        if(i-j+1>0)
+            Y(i)=Y(i)+X(j)*H(i-j+1);
+        else
+        end;
+    end;
 end;
